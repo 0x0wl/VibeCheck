225f407b46498e1c32ad7f50b5317653dec3933e
0x0wl <0x0wlet@gmail.com>
2020-10-17 15:32:11 -0400

    updated emotemodel and tweetprocessor

---
+++ EmoteModel.py
@@ -0,0 +1,41 @@
+from sklearn import preprocessing
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import SGDClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+import pandas
+import numpy
+import TweetProcessor
+
+dataset = pandas.read_csv('set.csv')
+dataset = dataset.drop('author', axis=1)
+
+dataset['content'] = dataset['content'].apply(TweetProcessor.process)
+
+#debugging
+print("loaded dataset...")
+
+
+label_encoder = preprocessing.LabelEncoder()
+y = label_encoder.fit_transform(dataset.sentiment.values)
+
+X_train, X_val, y_train, y_val = train_test_split(dataset.content.values, y, stratify=y, random_state=23, test_size=0.1, shuffle=True) #test_size=0.1?
+
+tf_idf_params = TfidfVectorizer(ngram_range=(1,3))
+XtIdf = tf_idf_params.fit_transform(X_train)
+XvIdf = tf_idf_params.fit_transform(X_val)
+
+count_vectors = CountVectorizer()
+count_vectors.fit(data['content'])
+XtCount = count_vect.transform(X_train)
+XvCount = count_vect.transform(X_val)
+
+#debugging
+print("loaded data for gradient descent...")
+
+
+lsvm = SGDClassifier(random_state=9, max_iter=15, tol=None)
+lsvm.fit(XtCount, y_train)
+y_pred = lsvm.predict(XvCount)
+print('lsvm using count vectors accuracy %s' % accuracy_score(y_pred, y_val))
--- TweetProcessor.py
+++ TweetProcessor.py
@@ -2,6 +2,7 @@ import os
 import urllib.parse
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
+import nltk
 import string
 import re
 
@@ -9,15 +10,15 @@ def process(s):
     # converts to lowercase
     s = s.lower()
     # removes punctuation
-    s.translate(string.maketrans('', '', string.punctuation))
+    s = re.sub(r'[^\w\s]', '', s)
     # removes stop words
-    s = " ".join([word for word in ntlk.tokenize.word_tokenize(s) if not word in stopwords.words("english")])
+    s = " ".join([word for word in nltk.tokenize.word_tokenize(s) if not word in stopwords.words("english")])
     # converts words to root words (stemming)
     porter = PorterStemmer()
-    s = " ".join([porter.stem(word) for word in ntlk.tokenize.word_tokenize(s)])
+    s = " ".join([porter.stem(word) for word in nltk.tokenize.word_tokenize(s)])
     # removes URLs
     s = " ".join(re.sub("([^0-9A-Za-z \t])|(\w+:\/\/\S+)", "", s).split())
     # removes usernames
-    s = " ".join(word for word in ntlk.tokenize.word_tokenize(s) if (not word.startsWith("@") and len(word) > 1))
+    s = " ".join(word for word in nltk.tokenize.word_tokenize(s) if (not word.startswith("@") and len(word) > 1))
     # removes three-char repitions
     s = re.compile(r"(.)\1{2,}").sub(r"\1\1", s)
