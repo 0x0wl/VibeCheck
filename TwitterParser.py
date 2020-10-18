ba9735a6412498406001ae5e19120e9a2c46c257
fuzzyhappy <student.e.wang@gmail.com>
2020-10-18 00:56:53 -0400

    12:13 am commit whooo

--- TwitterParser.py
+++ TwitterParser.py
@@ -19,7 +19,7 @@ myStream = tweepy.Stream(auth = api.auth, listener=myStreamListener())
 def fetchTweets(language, date_lim, numTweets):
     # query encoded to acceptable URL query
     # query = "+".join([urllib.parse.quote(s) for s in query.split()])
-    query = '(the OR i OR to OR a OR and OR is OR in OR it OR you OR of OR tinyurl.com OR for OR on OR my OR ‘s OR that OR at OR with OR me OR do OR have OR just OR this OR be OR n’t OR so OR are OR ‘m OR not OR was OR but OR out OR up OR what OR now OR new OR from OR your OR like OR good OR no OR get OR all OR about OR we OR if OR time OR as OR day OR will OR one OR twitter OR how OR can OR some OR an OR am OR by OR going OR they OR go OR or OR has OR rt OR know OR today OR there OR love OR more OR work OR too OR got OR he OR 2 OR back OR think OR did OR lol OR when OR see OR really OR had OR great OR off OR would OR need OR here OR thanks OR been OR blog OR still OR people OR who OR night OR ‘ll OR want OR why OR bit.ly OR home)'
+    query = '(i OR to OR a OR and OR is OR in OR it OR you OR of OR tinyurl.com OR for OR on OR my OR ‘s OR that OR at OR with OR me OR do OR have OR just OR this OR be OR n’t OR so)'
 
     # fetches an iterable collection of the
     # - specified number of tweets (numTweets)
